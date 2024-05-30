507c94b66d7349ee52814e8d2b4cd3cd789de967
Jiho Park <pakrjiho8966@gmail.com>
2024-05-30 10:16:15 -0400

    third trial hw1

--- hello.py
+++ hello.py
@@ -2,4 +2,9 @@ def hello_world():
 	return "Hello World!"
 
 def hello_world_n(N):
-	return "Hello World! "*N
+	n_hello = ''
+	for i in range (0, N):
+		n_hello += "Hello World!"
+		if i != N-1:
+			n_hello += ' '
+	return n_hello
